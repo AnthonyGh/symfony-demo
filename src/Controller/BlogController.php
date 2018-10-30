23c3dc34b5a9c9d506cac5069df513ab93d12cd6
Anthony <anthony_ghelfo@yahoo.fr>
2018-10-30 17:49:23 +0100

    Second Commit

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -11,7 +11,7 @@ class BlogController extends AbstractController
 {
 
 	/**
-     * @Route("/accueil", name="accueil")
+     * @Route("/", name="accueil")
      */
     public function accueil()
     {
