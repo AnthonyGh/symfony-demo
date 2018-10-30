b375bcabc3140e61ad31425560e9861a34002ae3
Anthony <anthony_ghelfo@yahoo.fr>
2018-10-30 19:37:36 +0100

    Application fonctionnelle

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,10 +2,18 @@
 
 namespace App\Controller;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 use App\Entity\Article;
+
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 
 class BlogController extends AbstractController
 {
@@ -35,10 +43,28 @@ class BlogController extends AbstractController
     /**
      * @Route("/articles/{id}", name="article_show")
      */
-    public function showArticle(Article $article)
+    public function showArticle(Article $article, UserInterface $user, Request $request, ObjectManager $manager)
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $comment->setCreatedAt(new \DateTime())
+                    ->setArticle($article)
+                    //si l'utilisateur est logger, mettre getUsername() de l'objet $user de UserInterface
+                    ->setAuthor($user->getUsername());
+
+            $manager->persist($comment);
+            $manager->flush($comment);
+            return $this->redirectToRoute('article_show', ['id' => $article->getId()]);
+        }
+
         return $this->render('blog/article.html.twig', [
-        	'article' => $article
+            
+        	'article' => $article,
+            'commentForm' => $form->createView()
         ]);
     }
 
